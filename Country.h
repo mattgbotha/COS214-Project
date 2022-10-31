8248b650fa32f2d08753f500eb9e686aac6983a1
skitsbi <u04525087@tuks.co.za>
2022-10-31 11:32:03 +0200

    Documentation basics
    Country Documentation

--- Country.h
+++ Country.h
@@ -22,17 +22,27 @@ private:
 
 public:
 	Country();
+	/// @brief Instantiates Country Object
+	/// @param name Name of the country
+	/// @param numPeople Population of the country
 	Country(string name, int numPeople);
 	virtual ~Country();
-
+	/// @brief Getter for name
+	/// @return returns name variable as a string
 	string getName();
+	/// @brief Getter for numPeople
+	/// @return returns numPeople as int
 	int getNumPeople();
-	int getID();
+	/// @brief Setter for name Variable
+	/// @param name Country name
 	void setName(string name);
+	/// @brief Setter for numPeople,Country Population
+	/// @param numPeople Population to set
 	void setNumPeople(int numPeople);
-
+	/// @brief Pure Virtual attack() function
 	virtual void attack() = 0;
-	virtual void defend() = 0;
+	/// @brief Pure virtual Defend() function
+ 	virtual void defend() = 0;
 
 	// void notify();
 	// void attach(Observer obs);
