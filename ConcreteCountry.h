6b1785c79d6be77d2ff7b29debe0baa042fa3b15
skitsbi <u04525087@tuks.co.za>
2022-11-01 09:09:02 +0200

    Update ConcreteCountry.h
    Documentation

--- ConcreteCountry.h
+++ ConcreteCountry.h
@@ -7,10 +7,16 @@
 
 class ConcreteCountry : public Country {
 public:
+	/// @brief base Constructor for concreteCountry
 	ConcreteCountry();
+	/// @brief Paramaterized Constructor for ConcreteCountry
+	/// @param name Country Name as string
+	/// @param numPeople Country Population as int
 	ConcreteCountry(string name, int numPeople);
 	virtual ~ConcreteCountry();
+	/// @brief attack() method calls the act() method on all people in this countrys people[]
 	virtual void attack() override;	// Call act() method on each person in people[]
+	/// @brief defend() method calls the act() method on all people in this countrys people[]
 	virtual void defend() override;	// Call act() method on each person in people[]
 };
 
