bc70f26de7db372885b1f2b6077702b898f010bf
AmickeC <croucampa01@gmail.com>
2022-11-03 13:20:48 +0200

    WorkingTransportState.h updated

--- WorkingTransportState.h
+++ WorkingTransportState.h
@@ -7,9 +7,19 @@
 
 #include "TransportState.h"
 
+/** @author Amicke
+ *  @date 2022/11/01
+ *
+ *  A specialization of the TransportState class - transport is in the working state
+ */
+
 class WorkingTransportState : public TransportState{
 
 public:
+    /**
+    * @brief handles state of transport to be working when called
+    * @return int of 1
+    */
     float handle();
 };
 
