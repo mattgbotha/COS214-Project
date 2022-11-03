b9721c4f1dd0376a480c767021ff14f365ab2380
AmickeC <croucampa01@gmail.com>
2022-11-03 22:42:25 +0200

    stateMem updated

--- stateMem.h
+++ stateMem.h
@@ -5,8 +5,18 @@
 #ifndef COS214_PROJECT_STATEMEM_H
 #define COS214_PROJECT_STATEMEM_H
 
+/**
+ * @author Amicke
+ * @date 2022/11/03
+ *
+ * StateMem represents the transportstate of the country aswell as the saceCit
+ */
+
 class stateMem {
 private:
+    /**
+     * @param transportstate
+     */
     TransportState *transportstate;
   //  People *peoplearray;
    // Citizen saveCit;
@@ -15,17 +25,29 @@ private:
 
 public:
 
+    /**
+     * @brief constructor of SateMem to initialize the trasnportstate object and saceCit vector
+     * @param ts to initialize the transportstate object
+     * @param parray to initialize the vector array
+     */
     stateMem(TransportState *ts, vector<People*> parray) {
         transportstate = ts;
         saceCit = parray;
 
     }
 
+    /**
+     * @brief constructor to initialize a new object that is passed through as parameter
+     * @param s to initialize the stateMem object with the trasnportstate and saceCit
+     */
     stateMem(const stateMem &s) {
         saceCit = s.saceCit;
         transportstate = s.transportstate;
     }
 
+    /**
+     * @brief loops through the saceCit while outputting each saceCit's state, the transportstate object of the country is then printed
+     */
     void showstate()
     {
         cout<<"States: "<<endl;
