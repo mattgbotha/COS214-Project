3a0ccb66dcabc56da90505ddfe9bf12e01d4bd2e
gryffindor-coder <wiandtoit@gmail.com>
2022-11-04 16:54:07 +0200

    Added documentation blocks for WarPhase

--- WarPhase.h
+++ WarPhase.h
@@ -7,6 +7,15 @@
 #include "WarEngine.h"
 #include <random>
 
+/** @author Ethan
+ *  @date 31 October 2022
+ *
+ *  The interface/abstract class for the phase the war is in (early/middle/late)
+ *
+ *  @see WarPhaseEarly
+ *  @see WarPhaseMiddle
+ *  @see WarPhaseLate
+ */
 class WarPhase{
 public:
     virtual ~WarPhase();
