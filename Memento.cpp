9cd6caca1cacf3d12d6d4572b8f37d8874c9687a
AmickeC <croucampa01@gmail.com>
2022-11-04 16:42:35 +0200

    Memento updated

--- Memento.cpp
+++ Memento.cpp
@@ -4,10 +4,16 @@
 
 #include "Memento.h"
 
+/**
+ * @brief the stateMem object is being initialise by the TransportState object and the vector array
+ */
 Memento::Memento() {
     state = new stateMem(ts,peparray);
 }
 
+/**
+ * @brief stateMem object will be deleted and state will be set to 0
+ */
 Memento::~Memento() {
     delete state;
     state = 0;
--- Memento.h
+++ Memento.h
@@ -9,18 +9,31 @@
 #include "stateMem.h"
 #include "Country.h"
 
+/**
+ * @author Amicke
+ * @date 2022/11/03
+ */
 
 class Memento {
 private:
-
+    /**
+     * @param ts - TransportState object
+     */
     TransportState* ts;
+    /**
+     * @param peparray - vector of People objects
+     */
     vector<People*> peparray;
      friend class WarEngine;
-
+    /**
+     * @brief Memento constructor to initialise a new stateMem object
+     */
     Memento();
+    /**
+     * @param state - stateMem object
+     */
     stateMem* state;
 public:
-
     virtual ~Memento();
 
 
